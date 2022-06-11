8069766b1d409b579e68db95f40f1e5a5b428abe
James Hodgkinson <james@terminaloutcomes.com>
2022-06-11 21:01:14 +1000

    fixing up tests

--- tests/test_http.py
+++ tests/test_http.py
@@ -15,10 +15,10 @@ def test_http_importer(tmp_path: Path) -> None:
     "source" : "https://raw.githubusercontent.com/yaleman/fail2ban-importer/main/example.json"
     }
 
-    p = tmp_path / "hello.txt"
-    p.write_text(json.dumps(config_file))
+    config_path = tmp_path / "hello.txt"
+    config_path.write_text(json.dumps(config_file))
 
-    result =fail2ban_importer.downloaders.http.download(str(p))
+    result =fail2ban_importer.downloaders.http.download(str(config_path))
 
+    assert result is not None
     assert result.data[0].ip == "192.168.255.255"
-
